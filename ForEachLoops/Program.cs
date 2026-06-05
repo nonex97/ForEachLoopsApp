c5b3930a14c003d689f9d2db797f55800fab24b2
nonex97 <ngrguric97@gmail.com>
2026-06-05 22:51:05 +0200

    Learned about foreach and how to use it.

--- ForEachLoops/Program.cs
+++ ForEachLoops/Program.cs
@@ -1,4 +1,9 @@
 ﻿
 
-s
+string data = "Tim,Sue,Bob,Jane";
+List<string> firstNames = data.Split(',').ToList();
 
+foreach (var firstName in firstNames) // var will automatically assign a string in this case (it is not variable)
+{
+    Console.WriteLine(firstName);
+}
